53c734be233e8b70d514638518b3af980b138192
Damion M Broadaway <lashner@gmail.com>
2013-09-16 14:24:16 -0500

    First!

---
+++ WPSettingsBuilder.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TODO: description for WPSettingsBuilder.php
+ *
+ * @package    WPSettingsBuilder
+ * @subpackage WPSettingsBuilder.php
+ * @version    $Id$
+ * @created    9/16/13 at 2:23 PM
+ */
+
+/**
+ * TODO: description for WPSettingsBuilder:WPSettingsBuilder
+ *
+ * @package    WPSettingsBuilder
+ * @subpackage WPSettingsBuilder
+ * @version    $Id$
+ * @author     Damion M Broadaway <dbroadaw@nerdery.com>
+ * @created    9/16/13 at 2:23 PM
+ */
+class WPSettingsBuilder
+{
+    public function __construct()
+    {
+    
+    }
+ 
+}
