5dc8d5623af0c8252083b15bfcb9cc016e72f854
Damion M Broadaway <lashner@gmail.com>
2013-11-15 16:09:50 -0600

    adding exception handling class

---
+++ WPSettingsBuilder_Errors.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * TODO: description for WPSettingsBuilder_Errors.php
+ *
+ * @package    wplatest-wpsettingsbuilder
+ * @subpackage WPSettingsBuilder_Errors.php
+ * @version    $Id$
+ * @created    11/15/13 at 4:07 PM
+ */
+
+/**
+ * TODO: description for wplatest-wpsettingsbuilder:WPSettingsBuilder_Errors
+ *
+ * @package    wplatest-wpsettingsbuilder
+ * @subpackage WPSettingsBuilder_Errors
+ * @version    $Id$
+ * @author     Damion M Broadaway <dbroadaw@nerdery.com>
+ * @created    11/15/13 at 4:07 PM
+ */
+class WPSettingsBuilder_Errors extends Exception
+{
+
+} 
